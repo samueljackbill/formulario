11dec80de0c8a7c1de92d927d5064f1667c9171b
Samuel Pires <samueljackbill@gmail.com>
2025-02-25 09:03:38 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Preencher variáveis pelos dados do Formulário
+document.getElementById('btnInscrever').addEventListener('click', function() {
+    var email = document.getElementById('inputEmail4').value;
+
+    alert("O seu e-mail é: " + email);
+})
